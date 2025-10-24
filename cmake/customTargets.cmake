154a3eefcf17c4acecb374393d9461bf4879c445
Jannis Sauer <jannis_sauer@icloud.com>
2025-10-24 23:12:30 +0200

    combined the 2 graphviz targets to 1

--- cmake/customTargets.cmake
+++ cmake/customTargets.cmake
@@ -7,24 +7,19 @@
 #
 
 
-# generate dependency graph
-add_custom_target(graph
-        COMMAND ${CMAKE_COMMAND}
-        --graphviz=${CMAKE_BINARY_DIR}/generated/dependencies.dot
-        ${CMAKE_SOURCE_DIR}
-        COMMENT "Generating CMake dependency graph (.dot file)..."
-)
-
 # check for graphviz
 find_program(GRAPHVIZ_DOT_EXECUTABLE dot)
 
 # auto-render PNG if Graphviz is available
 if(GRAPHVIZ_DOT_EXECUTABLE)
-    add_custom_target(graph_png
-        COMMAND ${CMAKE_COMMAND}
-        --graphviz=${CMAKE_BINARY_DIR}/generated/dependencies.dot
-        ${CMAKE_SOURCE_DIR}
-        COMMAND ${GRAPHVIZ_DOT_EXECUTABLE} -Tpng ${CMAKE_BINARY_DIR}/generated/dependencies.dot -o ${CMAKE_BINARY_DIR}/generated/dependencies.png
-        COMMENT "Generating dependency graph as PNG..."
+    add_custom_target(graph
+            COMMAND ${CMAKE_COMMAND}
+            --graphviz=${CMAKE_BINARY_DIR}/generated/dependencies.dot
+            ${CMAKE_SOURCE_DIR}
+            COMMAND ${CMAKE_COMMAND}
+            --graphviz=${CMAKE_BINARY_DIR}/generated/dependencies.dot
+            ${CMAKE_SOURCE_DIR}
+            COMMAND ${GRAPHVIZ_DOT_EXECUTABLE} -Tpng ${CMAKE_BINARY_DIR}/generated/dependencies.dot -o ${CMAKE_BINARY_DIR}/generated/dependencies.png
+            COMMENT "Generating dependency graph..."
     )
 endif()
